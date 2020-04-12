fcf554f04273a44bcfbe4d41342ed65d94ba5e3e
tommylettieri <tommylettieri@gmail.com>
2020-04-11 23:52:33 -0400

    Added preliminary user model and initial sequelize logic

--- src/configurations.ts
+++ src/configurations.ts
@@ -1,6 +1,13 @@
 export default {
     server: {
-        port: process.env.port || 3000,
-        basePath: process.env.basePath || ''
+        port: process.env.SERVER_PORT || 3000,
+        basePath: process.env.SERVER_BASE_PATH || ''
+    },
+    db: {
+        host: process.env.DB_HOST || 'localhost',
+        name: process.env.DB_NAME || 'rederly',
+        user: process.env.DB_USER || 'postgres',
+        password: process.env.DB_PASSWORD || 'password',
+        logging: process.env.DB_LOGGING || false,
     }
 }

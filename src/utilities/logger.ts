245600cc3cce84fa6fbbfa154c2d9f81b54a0d72
tommylettieri <tommylettieri@gmail.com>
2020-04-17 23:03:29 -0400

    Added file logging

--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -1,6 +1,15 @@
 import winston = require('winston');
 const logger = winston.createLogger({
     transports: [
+        new winston.transports.File({
+            level:            'info',
+            filename:         './logs/all-logs.log',
+            handleExceptions: true,
+            // json:             true,
+            maxsize:          5242880, //5MB
+            maxFiles:         5,
+            // colorize:         false
+          }), 
         new winston.transports.Console()
     ]
 });

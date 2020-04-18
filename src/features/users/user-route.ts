86896a4941fc7e53974de9fc6d3cba51d0b67c1e
tommylettieri <tommylettieri@gmail.com>
2020-04-17 22:19:45 -0400

    Added routes for adding a user and registering a user

--- src/features/users/user-route.ts
+++ src/features/users/user-route.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-
+import configurations from '../../configurations';
+import userController from "./user-controller";
 const router = require('express').Router();
 
 router.post('/login',
@@ -13,5 +14,24 @@ router.post('/login',
         res.cookie('sessionToken', 'test', cookieOptions);
         return res.status(200).json({}); // TODO create a response
     });
+
+router.post('/register',
+// TODO add req validation
+// TODO add passport auth
+async (req: Request, res: Response) => {
+    const baseUrl = `${req.protocol}://${req.get('host')}/${configurations.server.basePath}`;
+    const newUser = await userController.registerUser({
+        userObject: req.body,
+        baseUrl
+    });
+    return res.status(200).json(newUser); // TODO create a response
+});
+
+router.get('/verify',
+// TODO add req validation
+// TODO add passport auth
+(req: Request, res: Response) => {
+    return res.status(200).json({}); // TODO create a response
+});
 
 module.exports = router;

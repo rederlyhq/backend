7d716e7cb23674b7b70a2e0c7488517497bfc8d2
red-tt-bear <red.tt.bear@gmail.com>
2020-04-25 00:17:07 -0400

    Added async handler to catch all errors

--- src/features/users/user-route.ts
+++ src/features/users/user-route.ts
@@ -8,11 +8,12 @@ import Boom = require("boom");
 import passport = require("passport");
 import { authenticationMiddleware } from "../../middleware/auth";
 import httpResponse from "../../utilities/http-response";
+import * as asyncHandler from 'express-async-handler'
 
 router.post('/login',
     validate(loginValidation),
     passport.authenticate('local'),
-    async (req: Request, res: Response, next: NextFunction) => {
+    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
         const newSession = await userController.login(req.body.email, req.body.password);
         if (newSession) {
             const cookieOptions = {
@@ -23,32 +24,33 @@ router.post('/login',
         } else {
             next(Boom.badRequest('Invalid login'));
         }
-    });
+    }));
 
 router.post('/register',
     validate(registerValidation),
-    async (req: Request, res: Response, next: NextFunction) => {
-        const baseUrl = `${req.protocol}://${req.get('host')}/${configurations.server.basePath}`;
-        const newUser = await userController.registerUser({
-            userObject: req.body,
-            baseUrl
-        });
-        next(httpResponse.Created('User registered successfully', newUser));
+    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+            const baseUrl = `${req.protocol}://${req.get('host')}/${configurations.server.basePath}`;
+            const newUser = await userController.registerUser({
+                userObject: req.body,
+                baseUrl
+            });
+            next(httpResponse.Created('User registered successfully', newUser));
     });
+    }));
 
 router.get('/verify',
     validate(verifyValidation),
-    (req: Request, res: Response, next: NextFunction) => {
-        userController.verifyUser(req.query.verify_token);
+    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+        await userController.verifyUser(req.query.verify_token);
         next(httpResponse.Ok("Verified"));
-    });
+    }));
 
 router.post('/logout',
     authenticationMiddleware,
-    async (req: any, res: Response, next: NextFunction) => {
+    asyncHandler(async (req: any, res: Response, next: NextFunction) => {
         await userController.logout(req.session.dataValues.uuid);
         res.clearCookie('sessionToken');
         next(httpResponse.Ok("Logged out"));
-    });
+    }));
 
 module.exports = router;

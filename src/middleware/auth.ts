7a1b50c58107baee3173d6d1040445b4a2b28d3d
tommylettieri <tommylettieri@gmail.com>
2020-05-01 23:40:25 -0400

    Linted middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -4,35 +4,19 @@ import logger from "../utilities/logger";
 import moment = require("moment");
 import passport = require("passport");
 import Session from "../database/models/session";
+import { Request, Response, NextFunction } from "express";
 const LocalStrategy = require('passport-local').Strategy;
 
-export const authenticationMiddleware = async (req: any, res: any, next: any) => {
-    if (!req.cookies || !req.cookies.sessionToken) {
-        return next(Boom.unauthorized());
-    }
-    //authenticate cookie
+export const validateSession = async (uuid: string): Promise<Session> => {
     try {
-        let session = await validateSession(req.cookies.sessionToken);
-        //Successful, add the user id to the session
-        req.session = session;
-        return next();
-    } catch (err) {
-        res.clearCookie('sessionToken');
-        return res.redirect(parseInt(err.output.statusCode), '/');
-    }
-};
-
-
-export const validateSession = async (uuid: string) => {
-    try {
-        let session = await userController.getSession(uuid);
+        const session = await userController.getSession(uuid);
         if (!session) {
-            let response = 'Invalid session';
+            const response = 'Invalid session';
             logger.warn(response);
             throw Boom.unauthorized(response);
         } else {
-            let timeNow = moment();
-            let expiresAt = moment(session.expires_at);
+            const timeNow = moment();
+            const expiresAt = moment(session.expires_at);
             if (timeNow.isAfter(expiresAt)) {
                 const response = 'Session expired';
                 logger.warn(response);
@@ -51,13 +35,31 @@ export const validateSession = async (uuid: string) => {
     }
 };
 
-passport.serializeUser(async (session: any, done) => {
+export const authenticationMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    if (!req.cookies || !req.cookies.sessionToken) {
+        return next(Boom.unauthorized());
+    }
+    //authenticate cookie
+    try {
+        const session = await validateSession(req.cookies.sessionToken);
+        //Successful, add the user id to the session
+        // TODO figure out session with request
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (req as any).session = session;
+        return next();
+    } catch (err) {
+        res.clearCookie('sessionToken');
+        return res.redirect(parseInt(err.output.statusCode), '/');
+    }
+};
+
+passport.serializeUser(async (session: Session, done) => {
     return done(null, session);
 });
 
-passport.deserializeUser(async (id: number, done) => {
+passport.deserializeUser(async (id: number, done: (err: Boom<null>, user?: unknown) => void): Promise<void> => {
     try {
-        let user = userController.getUserById(id);
+        const user = userController.getUserById(id);
         return done(null, user);
     } catch (err) {
         return done(err);
@@ -65,7 +67,7 @@ passport.deserializeUser(async (id: number, done) => {
 });
 
 
-passport.use(new LocalStrategy({ usernameField: "email" }, async (email: string, password: string, done: any) => {
+passport.use(new LocalStrategy({ usernameField: "email" }, async (email: string, password: string, done: (err: Boom<null>, user?: unknown) => void) => {
     // TODO track ip?
     try {
         const session: Session = await userController.login(email, password);

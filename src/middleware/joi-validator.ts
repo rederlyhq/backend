18f58cc6ffa78ef496d57a0a343e14245e32eab1
red-tt-bear <red.tt.bear@gmail.com>
2020-05-01 23:40:25 -0400

    Linted middleware

--- src/middleware/joi-validator.ts
+++ src/middleware/joi-validator.ts
@@ -1,26 +1,33 @@
 import * as Joi from "joi"
 import Boom = require("boom");
 import Extend = require("extend");
+import { NextFunction, Response } from "express";
 
-export default function validate(schema: any, options: any = {}) {
+export default function validate(schema: Joi.SchemaLike, options: Joi.ValidationOptions = {}): (req: Request, res: Response, next: NextFunction) => void  {
     options.abortEarly = false;
 
     // Using Request from express I get typescript errors
-    return function validateRequest(req: any, res: Response, next: any) {
-        let toValidate: any = {};
+    return function validateRequest(req: Request, res: Response, next: NextFunction): void {
+        // This is a generic callback which can be used for any object, thus this can be any as well
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const toValidate: any = {};
         if (!schema) {
             next();
         }
 
-        ['params', 'body', 'query'].forEach(function (key: string) {
-            if (schema[key]) {
-                toValidate[key] = req[key];
+        ['params', 'body', 'query'].forEach((key: string) => {
+            // Schemas are generic and thus can be treated as anys
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            if ((schema as any)[key]) {
+                // params, body and query exist for req, but generic for each cannot tell
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                toValidate[key] = (req as any)[key];
             }
         });
 
-        return Joi.validate(toValidate, schema, options, onValidationComplete);
-
-        function onValidationComplete(err: any, validated: any) {
+        // Again this function is generic, validated can be any object based on the arguements
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const onValidationComplete = (err: Joi.ValidationError, validated: any): void => {
             if (err) {
                 return next(Boom.badRequest(err.message, err.details));
             }
@@ -31,5 +38,6 @@ export default function validate(schema: any, options: any = {}) {
             return next();
         }
 
+        return Joi.validate(toValidate, schema, options, onValidationComplete);
     };
 }

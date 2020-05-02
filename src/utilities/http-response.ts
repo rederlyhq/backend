b5666608813a1a177c565d2c4dc538f6d7b16ab0
red-tt-bear <red.tt.bear@gmail.com>
2020-05-01 23:53:26 -0400

    Linted utilities

--- src/utilities/http-response.ts
+++ src/utilities/http-response.ts
@@ -1,12 +1,14 @@
 interface HttpResponse {
-    message?: string,
-    data?: any,
-    statusCode: number,
-    status: string
+    message?: string;
+    // This is a generic object type for passing data down to the user
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    data?: any;
+    statusCode: number;
+    status: string;
 }
 
 const createObject = (status: string, statusCode: number, message?: string, data?: any): HttpResponse => {
-    const resp:HttpResponse = {
+    const resp: HttpResponse = {
         statusCode,
         status
     }
@@ -20,7 +22,13 @@ const createObject = (status: string, statusCode: number, message?: string, data
 }
 
 export default {
-    Ok: (message?: string, data?: any) => createObject("Ok", 200, message, data),
-    Created: (message?: string, data?: any) => createObject("Created", 201, message, data),
-    Accepted: (message?: string, data?: any) => createObject("Accepted", 202, message, data),
+    // Data is any object that you want to pass to the user
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Ok: (message?: string, data?: any): HttpResponse => createObject("Ok", 200, message, data),
+    // Data is any object that you want to pass to the user
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Created: (message?: string, data?: any): HttpResponse => createObject("Created", 201, message, data),
+    // Data is any object that you want to pass to the user
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Accepted: (message?: string, data?: any): HttpResponse => createObject("Accepted", 202, message, data),
 };

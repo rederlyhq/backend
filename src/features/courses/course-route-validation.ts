ab64bb0fa69841668420e2a2e99304bfff6508c9
tommylettieri <tommylettieri@gmail.com>
2020-05-02 12:11:20 -0400

    Added university and instructor inference

--- src/features/courses/course-route-validation.ts
+++ src/features/courses/course-route-validation.ts
@@ -5,8 +5,6 @@ import * as Joi from 'joi';
 export const createCourseValidation = {
     body: {
         curriculum_id: Joi.number().required(),
-        instructor_id: Joi.number().required(),
-        university_id: Joi.number().required(),
         course_name: Joi.string().required(),
         course_code: Joi.string().required(),
         course_start: Joi.date().required(),

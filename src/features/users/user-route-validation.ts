a02a301033ae475d7d2be5a74a72d0750aec7f64
tommylettieri <tommylettieri@gmail.com>
2020-05-16 22:27:25 -0400

    Added ability to list user with a set of userIds Used that functionality to email a list of users by a given id set

--- src/features/users/user-route-validation.ts
+++ src/features/users/user-route-validation.ts
@@ -1,22 +1,47 @@
 import * as Joi from 'joi';
+import { ValidationObject } from '../../generic-interfaces/validation-object';
 
-export const registerValidation = {
+export const registerValidation: ValidationObject = {
+    params: {},
+    query: {},
     body: {
         username: Joi.string().required(),
         email: Joi.string().email().required(),
         password: Joi.string().required(),
-    }
+    },
 }
 
-export const loginValidation = {
+export const loginValidation: ValidationObject = {
+    params: {},
+    query: {},
     body: {
         email: Joi.string().email().required(),
         password: Joi.string().required(),
-    }
+    },
 }
 
-export const verifyValidation = {
+export const verifyValidation: ValidationObject = {
+    params: {},
     query: {
         verifyToken: Joi.string().required()
-    }
+    },
+    body: {}
+}
+
+export const listUsers: ValidationObject = {
+    params: {},
+    query: {
+        userIds: Joi.alternatives().try(Joi.array().items(Joi.number()), Joi.number()).optional()
+    },
+    body: {},
+}
+
+export const emailUsers: ValidationObject = {
+    params: {},
+    query: {},
+    body: {
+        userIds: Joi.array().items(Joi.number()).required(),
+        subject: Joi.string().required(),
+        content: Joi.string().required(),
+    },
 }
--- src/features/users/user-route.ts
+++ src/features/users/user-route.ts
@@ -1,9 +1,8 @@
 import { Request, Response, NextFunction } from "express";
-import configurations from '../../configurations';
 import userController from "./user-controller";
 const router = require('express').Router();
 import validate from '../../middleware/joi-validator'
-import { registerValidation, loginValidation, verifyValidation } from "./user-route-validation";
+import { registerValidation, loginValidation, verifyValidation, listUsers, emailUsers } from "./user-route-validation";
 import Boom = require("boom");
 import passport = require("passport");
 import { authenticationMiddleware } from "../../middleware/auth";
@@ -12,6 +11,7 @@ import * as asyncHandler from 'express-async-handler'
 import NoAssociatedUniversityError from "../../exceptions/no-associated-university-error";
 import AlreadyExistsError from "../../exceptions/already-exists-error";
 import Session from "../../database/models/session";
+import emailHelper from "../../utilities/email-helper";
 
 router.post('/login',
     validate(loginValidation),
@@ -70,6 +70,7 @@ router.get('/verify',
 
 router.post('/logout',
     authenticationMiddleware,
+    // validate(), // TODO: should we have a no param validate to block all parameters?
     asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
         // TODO fix Request object for session
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -80,9 +81,39 @@ router.post('/logout',
 
 router.get('/',
     authenticationMiddleware,
+    validate(listUsers),
     asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-        const users = await userController.list();
+        const users = await userController.list({
+            filters: {
+                // TODO set types in Request
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                userIds: (req.query as any).userIds
+            }
+        });
         next(httpResponse.Ok(null, users));
+    }));
+
+router.post('/email',
+    authenticationMiddleware,
+    validate(emailUsers),
+    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+        const users = await userController.list({
+            filters: {
+                userIds: req.body.userIds
+            }
+        });
+        // TODO switch this from loop to bcc?
+        const emailPromises = [];
+        for(let i = 0; i < users.length; i++) {
+            emailPromises.push(emailHelper.sendEmail({
+                content: req.body.content as string,
+                subject: req.body.subject as string,
+                email: users[i].email
+            }));
+        }
+        await Promise.all(emailPromises);
+        // TODO change this to indicate problems
+        next(httpResponse.Ok(null, users.map(user => user.id)));
     }));
 
 module.exports = router;

60723042756bd8254483c27b1f0d3688f265f1e4
red-tt-bear <red.tt.bear@gmail.com>
2020-05-17 02:38:23 -0400

    Cleaned course route validations

--- src/features/courses/course-route-validation.ts
+++ src/features/courses/course-route-validation.ts
@@ -1,6 +1,9 @@
 import * as Joi from 'joi';
+import { ValidationObject } from '../../generic-interfaces/validation-object';
 
-export const createCourseValidation = {
+export const createCourseValidation: ValidationObject = {
+    params: {},
+    query: {},
     body: {
         curriculumId: Joi.number().required(),
         name: Joi.string().required(),
@@ -15,12 +18,24 @@ export const createCourseValidation = {
 export const getCourseValidation = {
     params: {
         id: Joi.number().required(),
+        query: {},
+        body: {},
     }
 }
 
 export const enrollInCourseValidation = {
+    params: {},
+    query: {},
     body: {
         courseId: Joi.number().required(),
         userId: Joi.number().required()
     }
+}
+
+export const listCoursesValidation = {
+    params: {},
+    query: {
+        instructorId: Joi.string().optional()
+    },
+    body: {},
 }

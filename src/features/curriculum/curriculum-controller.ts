f94c189199e84f9aa65d93efa4970acd4d60c78d
red-tt-bear <red.tt.bear@gmail.com>
2020-05-23 20:41:49 -0400

    Added create topic call

--- src/features/curriculum/curriculum-controller.ts
+++ src/features/curriculum/curriculum-controller.ts
@@ -2,6 +2,7 @@ import Bluebird = require('bluebird');
 import Curriculum from '../../database/models/curriculum';
 import UniversityCurriculumPermission from '../../database/models/university-curriculum-permission';
 import CurriculumUnitContent from '../../database/models/curriculum-unit-content';
+import CurriculumTopicContent from '../../database/models/curriculum-topic-content';
 
 class CurriculumController {
     getCurriculumById(id: number): Bluebird<Curriculum> {
@@ -26,6 +27,10 @@ class CurriculumController {
 
     createUnit(unit: CurriculumUnitContent): Promise<CurriculumUnitContent> {
         return CurriculumUnitContent.create(unit);
+    }
+
+    createTopic(topic: CurriculumTopicContent): Promise<CurriculumTopicContent> {
+        return CurriculumTopicContent.create(topic);
     }
 }
 

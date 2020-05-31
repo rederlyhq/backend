c7a1168fac7513ea16baa8540683d260780dcf11
tommylettieri <tommylettieri@gmail.com>
2020-05-31 16:41:00 -0400

    Added proxy for static files from renderer

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 // This file is just an aggregation of passing the routes to the router
 /* eslint-disable @typescript-eslint/no-var-requires */
 import express = require('express');
+import * as proxy from 'express-http-proxy';
+import configurations from './configurations';
 
 const router = express.Router();
 
 router.use('/users', require('./features/users/user-route'));
 router.use('/courses', require('./features/courses/course-route'));
 router.use('/curriculum', require('./features/curriculum/curriculum-route'));
+
+router.use('/webwork2_files', proxy(configurations.renderer.url));
 
 module.exports = router;

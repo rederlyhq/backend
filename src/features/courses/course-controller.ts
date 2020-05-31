c51d619f4b5c7225b5fd238d3c56da3704589554
tommylettieri <tommylettieri@gmail.com>
2020-05-31 16:56:36 -0400

    Added Get Question

--- src/features/courses/course-controller.ts
+++ src/features/courses/course-controller.ts
@@ -6,6 +6,8 @@ import NotFoundError from '../../exceptions/not-found-error';
 import CourseUnitContent from '../../database/models/course-unit-content';
 import CourseTopicContent from '../../database/models/course-topic-content';
 import CourseWWTopicQuestion from '../../database/models/course-ww-topic-question';
+import rendererHelper from '../../utilities/renderer-helper';
+import StudentGrade from '../../database/models/student-grade';
 
 interface EnrollByCodeOptions {
     code: string;
@@ -17,6 +19,11 @@ interface CourseListOptions {
         instructorId: number;
         enrolledUserId: number;
     };
+}
+
+interface GetQuestionOptions {
+    userId: number;
+    questionId: number;
 }
 
 class CourseController {
@@ -81,6 +88,43 @@ class CourseController {
         return CourseWWTopicQuestion.create(question);
     }
 
+    async getQuestion(question: any): Promise<any> {
+        const courseQuestion = await CourseWWTopicQuestion.findOne({
+            where: {
+                id: question.questionId
+            }
+        });
+
+        let studentGrade: StudentGrade;
+        studentGrade = await StudentGrade.findOne({
+            where: {
+                userId: question.userId,
+                courseWWTopicQuestionId: question.questionId
+            }
+        });
+
+        if(studentGrade === null) {
+            studentGrade = await StudentGrade.create({
+                userId: question.userId,
+                courseWWTopicQuestionId: question.questionId,
+                randomSeed: Math.floor(Math.random() * 999999),
+                bestScore: 0,
+                numAttempts: 0,
+                firstAttempts: 0,
+                latestAttempts: 0,
+            });
+        }
+
+        const rendererQuestion = await rendererHelper.getProblem({
+            sourceFilePath: courseQuestion.webworkQuestionPath,
+            problemSeed: studentGrade.randomSeed,
+            formURL: question.formURL,
+        });
+        return {
+            // courseQuestion,
+            rendererQuestion
+        }
+    }
     getCourseByCode(code: string): Promise<Course> {
         return Course.findOne({
             where: {

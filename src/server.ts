f689b08b929e6e7c6c28ada9fcb524bb609c0854
tommylettieri <tommylettieri@gmail.com>
2020-05-31 16:41:29 -0400

    The renderer uses form post so added urlencoded parser

--- src/server.ts
+++ src/server.ts
@@ -34,6 +34,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(cookieParser());
 
 app.use(passport.initialize());

c50a51f7320cb91693a98f8bd0b7df94032a39ee
red-tt-bear <red.tt.bear@gmail.com>
2020-06-07 01:00:45 -0400

    Switched proxy to return html rather than the json due to iframe implementation limitations

--- src/features/courses/course-route.ts
+++ src/features/courses/course-route.ts
@@ -108,13 +108,14 @@ router.get('/question/:id',
                 userId: session.userId,
                 formURL: `${req.protocol}://${req.get('host')}${req.originalUrl}`
             });
-            next(httpResponse.Ok('Fetched question successfully', question));
-            
+            // next(httpResponse.Ok('Fetched question successfully', question));
+
             // If testing renderer integration from the browser without the front end simply return the rendered html
             // To do so first uncomment the below res.send and comment out the above next
             // Also when in the browser console add your auth token (`document.cookie = "sessionToken=UUID;`)
             // Don't forget to do this in post as well
-            // res.send(question.rendererData.renderedHTML);
+            // TODO switch back to json response, right now we don't use the extra data and the iframe implementation requires html passed back
+            res.send(question.rendererData.renderedHTML);
         } catch (e) {
             next(e);
         }
@@ -126,11 +127,11 @@ router.post('/question/:id',
         proxyReqPathResolver: (req) => {
             return `/rendered?${qs.stringify({
                 format: 'json',
-                template:'simple',
+                template: 'simple',
                 formURL: `${req.protocol}://${req.get('host')}${req.originalUrl}`,
             })}`;
         },
-        userResDecorator: async (proxyRes: Response<any>, proxyResData: any, userReq: any , userRes: Response<any>) => {
+        userResDecorator: async (proxyRes: Response<any>, proxyResData: any, userReq: any, userRes: Response<any>) => {
             let data = proxyResData.toString('utf8');
             data = JSON.parse(data);
 
@@ -143,15 +144,16 @@ router.post('/question/:id',
 
             // There is no way to get next callback, however anything thrown will get sent to next
             // Using the below line will responde with a 201 the way we do in our routes
-            throw httpResponse.Ok('Answer submitted for question', {
-                rendererData: data
-            });
+            // throw httpResponse.Ok('Answer submitted for question', {
+            //     rendererData: data
+            // });
 
             // If testing renderer integration from the browser without the front end simply return the rendered html
             // To do so first uncomment the below return and comment out the above throw
             // Also when in the browser console add your auth token (`document.cookie = "sessionToken=UUID;`)
             // Don't forget to do this in get as well
-            // return data.renderedHTML;
+            // TODO switch back to json response, right now we don't use the extra data and the iframe implementation requires html passed back
+            return data.renderedHTML;
         }
     }));
 

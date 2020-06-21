0b87bb44bac1a32f7ec2b12052e8b55e83156e3d
tommylettieri <tommylettieri@gmail.com>
2020-06-21 02:58:38 -0400

    #27 Added grade association to questions so we could get all grades down from a course

--- src/database/models/course-ww-topic-question.ts
+++ src/database/models/course-ww-topic-question.ts
@@ -1,7 +1,5 @@
 import { Model, DataTypes, BelongsToGetAssociationMixin } from 'sequelize';
 import appSequelize from '../app-sequelize'
-import CurriculumTopicContent from './curriculum-topic-content';
-import CourseTopicContent from './course-topic-content';
 
 export default class CourseWWTopicQuestion extends Model {
     public id!: number; // Note that the `null assertion` `!` is required in strict mode.
@@ -16,6 +14,23 @@ export default class CourseWWTopicQuestion extends Model {
     // timestamps!
     public readonly createdAt!: Date;
     public readonly updatedAt!: Date;
+
+    static createAssociations(): void {
+        // This is a hack to add the associations later to avoid cyclic dependencies
+        /* eslint-disable @typescript-eslint/no-use-before-define */
+        CourseWWTopicQuestion.belongsTo(CourseTopicContent, {
+            foreignKey: 'courseTopicContentId',
+            targetKey: 'id',
+            as: 'topic'
+        });
+
+        CourseWWTopicQuestion.hasMany(StudentGrade, {
+            foreignKey: 'courseWWTopicQuestionId',
+            sourceKey: 'id',
+            as: 'grades'
+        })
+        /* eslint-enable @typescript-eslint/no-use-before-define */
+    }
 }
 
 CourseWWTopicQuestion.init({
@@ -61,8 +76,6 @@ CourseWWTopicQuestion.init({
     sequelize: appSequelize, // this bit is important
 });
 
-CourseWWTopicQuestion.belongsTo(CourseTopicContent, {
-    foreignKey: 'courseTopicContentId',
-    targetKey: 'id',
-    as: 'topic'
-});
+import CurriculumTopicContent from './curriculum-topic-content';
+import CourseTopicContent from './course-topic-content';
+import StudentGrade from './student-grade';

552f9b418027d3dd3845a7cf74a4f7e3906a3b41
Thomas Lettieri <tommylettieri@gmail.com>
2020-07-18 22:59:16 -0400

    Fixed some sequelize error handling and queries that use the field name
    name

---
+++ src/exceptions/wrapped-error.ts
@@ -0,0 +1,33 @@
+import * as _ from "lodash";
+
+export default class WrappedError extends Error {
+    private cause?: Error;
+    private msg: string;
+    private _stack: string;
+
+    constructor(message: string, cause?: Error) {
+        super(message);
+        this.name = this.constructor.name;
+        this.msg = message;
+        this.cause = cause
+        this._stack = this.stack
+        // this.stack = "${this.stack}\nCaused by:\n${cause.stack}")
+        Object.defineProperty(this, 'stack', {
+            get: function() {
+                if(_.isNil(this.cause)) {
+                    return this._stack
+                } else {
+                    return `${this._stack}\nCaused by:\n${this.cause.stack}`;
+                }
+                // return 'extended ' + this._stack;
+            },
+            set: function(value) {
+                this._stack = value;
+            }
+        });
+    }
+
+    toJSON() {
+        return _.omit(this, '_stack')
+    }
+}

cd922e109b2b8f6353ee30a312b4d200e8b10440
red-tt-bear <red.tt.bear@gmail.com>
2020-07-26 02:07:15 -0400

    Issue #32 Added unique contraint error handling

--- src/features/curriculum/curriculum-controller.ts
+++ src/features/curriculum/curriculum-controller.ts
@@ -143,11 +143,25 @@ class CurriculumController {
     }
 
     async updateUnit(options: UpdateUnitOptions): Promise<number> {
-        const updates = await CurriculumUnitContent.update(options.updates, {
-            where: options.where
-        });
-        // updates count
-        return updates[0];
+        try {
+            const updates = await CurriculumUnitContent.update(options.updates, {
+                where: options.where
+            });
+            // updates count
+            return updates[0];
+        } catch(e) {
+            if (e instanceof UniqueConstraintError) {
+                // The sequelize type as original as error but the error comes back with this additional field
+                // To workaround the typescript error we must declare any
+                const violatedConstraint = (e.original as any).constraint
+                if (violatedConstraint === CurriculumUnitContent.constraints.uniqueNamePerCurriculum) {
+                    throw new AlreadyExistsError('A unit with this name already exists for this curriculum');
+                } else if (violatedConstraint === CurriculumUnitContent.constraints.uniqueOrderPerCurriculum) {
+                    throw new AlreadyExistsError('A unit with this order already exists for this curriculum');
+                }
+            }
+            throw new WrappedError("Unknown error occurred", e);
+        }
     }
 }
 

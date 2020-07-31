55a4241f1ba99b7be40b08bdb3ab52a335d24648
red-tt-bear <red.tt.bear@gmail.com>
2020-07-31 00:14:57 -0400

    Issue #24 Fixed type for RederlyExpressRequest

--- src/extensions/rederly-express-request.ts
+++ src/extensions/rederly-express-request.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction } from "express";
+import { Request } from "express";
 import * as core from "express-serve-static-core";
 import Session from "../database/models/session";
 
 export interface RederlyExpressRequest<P extends core.Params = core.ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = core.Query> extends Request<P , ResBody, ReqBody, ReqQuery> {
-    session?: {
+    session?: Session & {
         passport: {
             user: Session;
         };

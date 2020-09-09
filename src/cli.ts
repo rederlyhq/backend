bfa2506edf0ccf75c24c4525da6f28466ca4cdc0
red-tt-bear <red.tt.bear@gmail.com>
2020-09-08 20:53:44 -0400

    RED-1 Made CLI have a list of available commands
    Now instead of it just doing sync grades you have to tell it to
    `sync-missing-grades`

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@ import './extensions';
 import configurations from './configurations';
 // TODO change logger to just use console in this case
 import logger from './utilities/logger';
+import * as _ from 'lodash';
 
 const enableddMarker = new Array(20).join('*');
 const disableddMarker = new Array(20).join('#');
@@ -15,10 +16,27 @@ if (configurations.email.enabled) {
 import { sync } from './database';
 import courseController from './features/courses/course-controller';
 
-(async (): Promise<void> => {
-    await sync();
-
+const syncMissingGrades = async (): Promise<void> => {
     logger.info('Performing missing grade sync');
     await courseController.syncMissingGrades();
     logger.info('done');
+};
+
+const commandLookup: {[key: string]: () => unknown} = {
+    ['sync-missing-grades']: syncMissingGrades
+};
+
+(async (): Promise<void> => {
+    await sync();
+
+    logger.info('Running CLI');
+    const command = process.argv[2]?.toLowerCase();
+    logger.info(`Running command: ${command}`);
+    const commandFunction = commandLookup[command];
+    if (_.isNil(commandFunction)) {
+        logger.error(`"${command}" is not a valid command, it must be one of: ${JSON.stringify(Object.keys(commandLookup))}`);
+    } else {
+        await commandFunction();
+    }
+    logger.info('Finished running CLI');
 })();

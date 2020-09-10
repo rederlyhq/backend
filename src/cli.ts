dad78b046bdcb9c43abccff3179ff26c094ae64c
red-tt-bear <red.tt.bear@gmail.com>
2020-09-10 15:54:05 -0400

    RED-1 Fixed cli for the changes we've made for clean workbooks

--- src/cli.ts
+++ src/cli.ts
@@ -17,6 +17,7 @@ import { sync } from './database';
 import courseController from './features/courses/course-controller';
 import appSequelize from './database/app-sequelize';
 import StudentWorkbook from './database/models/student-workbook';
+import * as Joi from '@hapi/joi';
 import rendererHelper from './utilities/renderer-helper';
 
 const syncMissingGrades = async (): Promise<void> => {
@@ -30,11 +31,18 @@ const cleanupWorkbooks = async (): Promise<void> => {
         const workbooks = await StudentWorkbook.findAll();
         logger.info(`Workbook count: ${workbooks.length}`);
         await workbooks.asyncForEach(async (workbook: StudentWorkbook) => {
-            workbook.submitted = await rendererHelper.cleanSubmitResponseData(workbook.submitted);
-            
+            let submitted = workbook.submitted;
+            if (typeof (submitted) === 'string') {
+                submitted = JSON.parse(submitted);
+            }
+
+            submitted = rendererHelper.cleanRendererResponseForTheDatabase(submitted);
+
             // Form data can be anything, furthermore this is temporary since it is just for cleanup purposes, cleaning up some null bytes from an old version of the renderer
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            const formData = (workbook.submitted.form_data as any);
+            const formData = (submitted.form_data as { [key: string]: unknown });
+            if (_.isNil(formData)) {
+                throw new Error('Missing form data!');
+            }
             if (typeof(formData.submitAnswers) === 'string') {
                 formData.submitAnswers = formData.submitAnswers.substring(0, formData.submitAnswers.indexOf('\0'));
             }
@@ -42,8 +50,7 @@ const cleanupWorkbooks = async (): Promise<void> => {
                 formData.format = formData.format.substring(0, formData.format.indexOf('\0'));
             }
 
-            delete workbook.submitted.renderedHTML;
-
+            workbook.submitted = submitted;
             await workbook.save();
         });
     });

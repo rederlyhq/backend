88e8127d4d5646a688c7783df01f8f9135b1167a
red-tt-bear <red.tt.bear@gmail.com>
2020-10-01 20:14:47 -0400

    RED-100 Added test for has no score and got .5

--- src/utilities/grading-helper.test.ts
+++ src/utilities/grading-helper.test.ts
@@ -49,6 +49,7 @@ const defaultTopicUnusedFields = {
 };
 
 describe('Grading Helper Tests', () => {
+    const timeOfSubmission = new Date();
     describe('calculateGrade', () => {
         describe('On Time', () => {
             const startDate = moment().subtract(1, 'days');
@@ -56,19 +57,29 @@ describe('Grading Helper Tests', () => {
             const deadDate = moment().add(2, 'days');
             const solutionDate = moment().add(3, 'days');
 
+            const resultIsLate = false;
+            const resultIsExpired = false;
+            const resultIsOnTime = true;
+
             describe('Within attempt limit', () => {
                 const maxAttempts = 1;
                 const numAttempts = 0;
 
-                describe('Within attempt limit', () => {
+                const resultIsWithinAttemptLimit = true;
+
+                describe('Has no score', () => {
                     const bestScore = 0;
                     const effectiveScore = 0;
                     const legalScore = 0;
                     const overallBestScore = 0;
                     const partialCreditBestScore = 0;
 
+                    const resultIsCompleted = false;
+
                     describe('Grade unlocked', () => {
                         const locked = false;
+                        const resultIsLocked = false;
+
                         it('Scored 0', () => {
                             const newScore = 0;
                             const params: CalculateGradeOptions = {
@@ -88,7 +99,7 @@ describe('Grading Helper Tests', () => {
                                     partialCreditBestScore,
                                     locked,
                                 },
-                                timeOfSubmission: new Date(),
+                                timeOfSubmission,
                                 topic: {
                                     ...defaultTopicUnusedFields,
                                     startDate: startDate.toDate(),
@@ -100,19 +111,73 @@ describe('Grading Helper Tests', () => {
 
                             expect(result).toStrictEqual({
                                 gradingPolicy: {
-                                    isCompleted: false,
-                                    isExpired: false,
-                                    isLocked: false,
-                                    isWithinAttemptLimit: true,
-                                    isOnTime: true,
-                                    isLate: false,
+                                    isCompleted: resultIsCompleted,
+                                    isExpired: resultIsExpired,
+                                    isLocked: resultIsLocked,
+                                    isWithinAttemptLimit: resultIsWithinAttemptLimit,
+                                    isOnTime: resultIsOnTime,
+                                    isLate: resultIsLate,
                                     willTrackAttemptReason: 'YES',
                                     willGetCreditReason: 'YES'
                                 },
                                 // Nothing should be updated since nothing improved
                                 gradeUpdates: {},
                                 // Should match score from params
-                                score: 0
+                                score: newScore
+                            });
+                        });
+
+                        it('Scored .5', () => {
+                            const newScore = .5;
+                            const params: CalculateGradeOptions = {
+                                newScore,
+                                question: {
+                                    ...defaultQuestionUnusedFields,
+                                    maxAttempts,
+                                },
+                                solutionDate,
+                                studentGrade: {
+                                    ...defaultStudentGradeUnusedFields,
+                                    numAttempts,
+                                    bestScore,
+                                    effectiveScore,
+                                    legalScore,
+                                    overallBestScore,
+                                    partialCreditBestScore,
+                                    locked,
+                                },
+                                timeOfSubmission,
+                                topic: {
+                                    ...defaultTopicUnusedFields,
+                                    startDate: startDate.toDate(),
+                                    endDate: endDate.toDate(),
+                                    deadDate: deadDate.toDate(),
+                                }
+                            };
+                            const result = calculateGrade(params);
+
+
+                            expect(result).toStrictEqual({
+                                gradingPolicy: {
+                                    isCompleted: resultIsCompleted,
+                                    isExpired: resultIsExpired,
+                                    isLocked: resultIsLocked,
+                                    isWithinAttemptLimit: resultIsWithinAttemptLimit,
+                                    isOnTime: resultIsOnTime,
+                                    isLate: resultIsLate,
+                                    willTrackAttemptReason: 'YES',
+                                    willGetCreditReason: 'YES'
+                                },
+                                // Nothing should be updated since nothing improved
+                                gradeUpdates: {
+                                    bestScore: newScore,
+                                    effectiveScore: newScore,
+                                    legalScore: newScore,
+                                    overallBestScore: newScore,
+                                    partialCreditBestScore: newScore
+                                },
+                                // Should match score from params
+                                score: newScore
                             });
                         });
                     });

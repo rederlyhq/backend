63c8d76aa0d40067cd0843a49fdae25b48612d47
red-tt-bear <red.tt.bear@gmail.com>
2020-10-16 01:41:14 -0400

    RED-117 Added some test code

--- src/playground.ts
+++ src/playground.ts
@@ -2,6 +2,7 @@ import configurations from './configurations';
 import './extensions';
 // TODO change logger to just use console in this case
 import logger from './utilities/logger';
+import * as moment from 'moment';
 
 const enableddMarker = new Array(20).join('*');
 const disableddMarker = new Array(20).join('#');
@@ -12,10 +13,39 @@ if (configurations.email.enabled) {
 }
 
 import { sync } from './database';
+import schedulerHelper, { HttpMethod } from './utilities/scheduler-helper';
 
 (async (): Promise<void> => {
     await sync();
 
     logger.info('Playground start');
+    const resultPromises: Array<Promise<unknown>> = []; 
+    for (let i = 0; i < 100; i++) {
+        try {
+            // const time = moment().toDate();
+            const time = moment().add(1, 'minute').toDate();
+            const id = `Iteration: ${i}; Date: ${new Date().toString()}`;
+            logger.info(id);
+            const resultPromise = schedulerHelper.setJob({
+                id: id,
+                time: time,
+                webHookScheduleEvent: {
+                    method: HttpMethod.POST,
+                    url: 'http://172.17.0.1:3001/backend-api/schedule/hook',
+                    data: {
+                        id: id,
+                        time: time,
+                    }
+                }
+            });
+            resultPromise.then((data: any) => {
+                console.log(data.data);
+            });
+            resultPromises.push(resultPromise);
+        } catch (e) {
+            logger.error(e.message);
+        }
+    }
+    await Promise.all(resultPromises);
     logger.info('Playground done');
 })();

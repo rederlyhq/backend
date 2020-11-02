a89a52f0fd8de8e541574b314a7437dfd6eaecb7
red-tt-bear <red.tt.bear@gmail.com>
2020-11-02 14:42:42 -0500

    RED-262 add global event listeners for errors
    If a promise or an error go unhandled we log the error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import configurations from './configurations';
 import './extensions';
 import logger from './utilities/logger';
+import './global-error-handlers';
+
 const enabledMarker = new Array(20).join('*');
 const disabledMarker = new Array(20).join('#');
 if (configurations.email.enabled) {

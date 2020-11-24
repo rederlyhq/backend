68abd00a2b81994e197966a695d276d95592089b
red-tt-bear <red.tt.bear@gmail.com>
2020-11-23 20:16:37 -0500

    Added a script to verify that the build runs
    This script imports the crux of the application but does not do the
    server listen
    
    Optionally it can sync with the database but by default it just imports
    and exits
    
    For use with build check

---
+++ src/test-run.ts
@@ -0,0 +1,31 @@
+import configurations from './configurations';
+import './extensions';
+import logger from './utilities/logger';
+import './global-error-handlers';
+
+const enabledMarker = new Array(20).join('*');
+const disabledMarker = new Array(20).join('#');
+if (configurations.email.enabled) {
+    logger.info(`${enabledMarker} EMAIL ENABLED ${enabledMarker}`);
+} else {
+    logger.info(`${disabledMarker} EMAIL DISABLED ${disabledMarker}`);
+}
+
+import { sync } from './database';
+import './server';
+
+(async (): Promise<void> => {
+    try {
+        const firstArg = process.argv[2];
+        if (firstArg === 'sync') {
+            logger.info(`${enabledMarker} "${firstArg}" === "sync"; Running sync ${enabledMarker}`);
+            await sync();
+        } else {
+            logger.info(`${disabledMarker} "${firstArg}" !== "sync"; Skipping sync ${disabledMarker}`);
+        }    
+    } catch (e) {
+        logger.error(`Could not start up ${e}`);
+        // Used a larger number so that we could determine by the error code that this was an application error
+        process.exit(87);
+    }
+})();

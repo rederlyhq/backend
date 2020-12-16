e402d729fdf3898133cf76b098275a7c92763bac
red-tt-bear <red.tt.bear@gmail.com>
2020-12-16 14:05:43 -0500

    RED-10 Moved file discovery from def file into another method

--- src/utilities/webwork-utilities/importer.ts
+++ src/utilities/webwork-utilities/importer.ts
@@ -28,13 +28,13 @@ const getContentRoot = async (filePath: string): Promise<string> => {
             const templatesPath = path.join(filePath, files[0], 'templates');
             const templatesDirStat = await fsPromises.lstat(templatesPath);
             if (templatesDirStat.isDirectory()) {
-                return templatesPath;
+                return path.resolve(templatesPath);
             }
         } catch (e) {
             logger.error('Could not stat templates', e);
         }
     }
-    return filePath;
+    return path.resolve(filePath);
 };
 
 interface FindFilesAssetFileResult {
@@ -58,54 +58,58 @@ interface FindFilesResult {
     defFiles: { [key: string]: FindFilesDefFileResult };
 }
 
+export const findFilesFromDefFile = async (contentRootPath: string, defFilePath: string): Promise<FindFilesDefFileResult> => {
+    const defFileRelativePath = path.relative(contentRootPath, defFilePath);
+    const defFileResult: FindFilesDefFileResult = {
+        defFileAbsolutePath: defFilePath,
+        defFileRelativePath: defFileRelativePath,
+        pgFiles: {}
+    };
+    const defFileContent = (await fsPromises.readFile (defFilePath)).toString();
+    const pgFileInDefFileMatches = getAllMatches(pgFileInDefFileRegex, defFileContent);
+    await pgFileInDefFileMatches.asyncForEach(async (pgFileInDefFileMatch) => {
+        const pgFilePathFromDefFile = pgFileInDefFileMatch[1];
+        const pgFilePath = path.join(contentRootPath, pgFilePathFromDefFile);
+        const pgFileResult: FindFilesPGFileResult = {
+            pgFilePathFromDefFile: pgFilePathFromDefFile,
+            pgFilePathOnDisk: pgFilePath,
+            assetFiles: {}
+        };
+        defFileResult.pgFiles[pgFilePathFromDefFile] = pgFileResult;
+        try {
+            const pgFileStats = await fsPromises.lstat(pgFilePath);
+            if (!pgFileStats.isFile()) {
+                throw new RederlyError(`${pgFilePath} is not a file`);
+            }
+            const pgFileContent = (await fsPromises.readFile(pgFilePath)).toString();
+            const imageInPGFileMatches = getAllMatches(imageInPGFileRegex, pgFileContent);
+            await imageInPGFileMatches.asyncForEach(async (imageInPGFileMatch) => {
+                const imagePath = imageInPGFileMatch[1] ?? imageInPGFileMatch[2];
+                const assetFileResult: FindFilesAssetFileResult = {
+                    assetFilePathFromPgFile: imagePath,
+                    assetFilePath: path.join(path.dirname(pgFilePath), imagePath),
+                };
+                pgFileResult.assetFiles[imagePath] = assetFileResult;
+                console.log(`imagePath: ${imagePath}`);
+            });
+        } catch (e) {
+            // logger.error(`Could not find pg file ${pgFilePath}`, e);
+        }
+        // console.log(pgFilePath);
+    });
+    return defFileResult;
+};
+
 export const findFiles = async (filePath: string): Promise<FindFilesResult> => {
     const contentRootPath = await getContentRoot(filePath);
 
-    // return recursiveListFilesInDirectory(filePath, [], listFilters.matches(/\.def$|\.pg/i));
-    const defFiles = await findDefFiles(filePath);
+    const defFiles = await findDefFiles(contentRootPath);
     const result: FindFilesResult = {
         defFiles: {}
     };
     await defFiles.asyncForEach(async (defFilePath) => {
-        const defFileRelativePath = path.relative(filePath, defFilePath);
-        const defFileResult: FindFilesDefFileResult = {
-            defFileAbsolutePath: defFilePath,
-            defFileRelativePath: defFileRelativePath,
-            pgFiles: {}
-        };
-        result.defFiles[defFileRelativePath] = defFileResult;
-        const defFileContent = (await fsPromises.readFile (defFilePath)).toString();
-        const pgFileInDefFileMatches = getAllMatches(pgFileInDefFileRegex, defFileContent);
-        await pgFileInDefFileMatches.asyncForEach(async (pgFileInDefFileMatch) => {
-            const pgFilePathFromDefFile = pgFileInDefFileMatch[1];
-            const pgFilePath = path.join(contentRootPath, pgFilePathFromDefFile);
-            const pgFileResult: FindFilesPGFileResult = {
-                pgFilePathFromDefFile: pgFilePathFromDefFile,
-                pgFilePathOnDisk: pgFilePath,
-                assetFiles: {}
-            };
-            defFileResult.pgFiles[pgFilePath] = pgFileResult;
-            try {
-                const pgFileStats = await fsPromises.lstat(pgFilePath);
-                if (!pgFileStats.isFile()) {
-                    throw new RederlyError(`${pgFilePath} is not a file`);
-                }
-                const pgFileContent = (await fsPromises.readFile(pgFilePath)).toString();
-                const imageInPGFileMatches = getAllMatches(imageInPGFileRegex, pgFileContent);
-                await imageInPGFileMatches.asyncForEach(async (imageInPGFileMatch) => {
-                    const imagePath = imageInPGFileMatch[1] ?? imageInPGFileMatch[2];
-                    const assetFileResult: FindFilesAssetFileResult = {
-                        assetFilePathFromPgFile: imagePath,
-                        assetFilePath: path.join(path.dirname(pgFilePath), imagePath),
-                    };
-                    pgFileResult.assetFiles[imagePath] = assetFileResult;
-                    console.log(`imagePath: ${imagePath}`);
-                });
-            } catch (e) {
-                // logger.error(`Could not find pg file ${pgFilePath}`, e);
-            }
-            // console.log(pgFilePath);
-        });
+        const defFileResult = await findFilesFromDefFile(contentRootPath, defFilePath);
+        result.defFiles[defFileResult.defFileRelativePath] = defFileResult;
     });
     return result;
 };

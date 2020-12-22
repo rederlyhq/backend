95df83320e4744efc9f76f35d4639b59d755b343
Victor Von Doom <68438287+doombot-exe@users.noreply.github.com>
2020-12-22 15:13:40 -0500

    block CUD operations on courses for unpaid users

--- src/extensions/rederly-express-request.ts
+++ src/extensions/rederly-express-request.ts
@@ -1,16 +1,18 @@
 import { Request } from 'express';
 import * as core from 'express-serve-static-core';
 import Session from '../database/models/session';
+import User from '../database/models/user';
 
 // This is the default typing in express which we are extending
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export interface RederlyExpressRequest<P extends core.Params = core.ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = core.Query, MetaType = any> extends Request<P, ResBody, ReqBody, ReqQuery> {
+export interface RederlyExpressRequest<P extends core.Params = core.ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = core.Query, MetaType = any > extends Request<P, ResBody, ReqBody, ReqQuery> {
     session?: Session & {
         passport: {
             user: Session;
         };
         dataValues: Session;
     };
+    rederlyUser?: User;
     // Any information already defined in the request
     meta?: MetaType;
     requestId?: string;

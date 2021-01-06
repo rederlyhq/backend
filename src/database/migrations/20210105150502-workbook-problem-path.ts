9171ce8190c5a656a53874900f22841471c6bb28
red-tt-bear <red.tt.bear@gmail.com>
2021-01-05 21:19:29 -0500

    RED-322 Added migration for problem path in workbooks
    This is an audit field in case a professor alters a student's problem
    path

---
+++ src/database/migrations/20210105150502-workbook-problem-path.ts
@@ -0,0 +1,48 @@
+import { QueryInterface, DataTypes } from 'sequelize';
+
+export default {
+  up: async (queryInterface: QueryInterface): Promise<void> => {
+    // Transactions are automatically use because a namespace is injected into sequelize when fetching configurations
+    await queryInterface.sequelize.transaction(async () => {
+      // TODO
+      await queryInterface.addColumn('student_workbook', 'student_workbook_problem_path', {
+        type: DataTypes.TEXT,
+        // Have to add later after filling previous
+        // allowNull: false,
+      });
+
+      await queryInterface.sequelize.query(`
+        UPDATE student_workbook
+        SET student_workbook_problem_path = course_topic_question.course_topic_question_webwork_question_ww_path
+        FROM student_grade
+        INNER JOIN course_topic_question ON course_topic_question.course_topic_question_id = student_grade.course_topic_question_id
+        WHERE student_workbook.student_grade_id = student_grade.student_grade_id
+        AND student_workbook.student_grade_instance_id IS NULL;
+      `);
+
+      await queryInterface.sequelize.query(`
+        UPDATE student_workbook
+        SET student_workbook_problem_path = student_grade_instance.student_grade_instance_problem_path
+        FROM student_grade_instance
+        WHERE student_workbook.student_grade_instance_id = student_grade_instance.student_grade_instance_id;
+      `);
+
+      await queryInterface.changeColumn(
+        'student_workbook',
+        'student_workbook_problem_path',
+        {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        }
+      );
+    });
+  },
+  down: async (queryInterface: QueryInterface): Promise<void> => {
+    // Transactions are automatically use because a namespace is injected into sequelize when fetching configurations
+    await queryInterface.sequelize.transaction(async () => {
+        // TODO
+        await queryInterface.removeColumn('student_workbook', 'student_workbook_problem_path');
+
+    });
+  }
+};

099e2ddf130f1a8dd3bce56656bb52f849fd7804
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2021-01-13 09:57:09 -0500

    Fixed bug with course creation from curriculum.
    I apparently can't read what I'm assigning to objects. :facepalm:

--- src/features/courses/course-controller.ts
+++ src/features/courses/course-controller.ts
@@ -364,7 +364,7 @@ class CourseController {
                             await TopicAssessmentInfo.create({
                                 ..._.omit(topicAssessmentInfo.get({plain: true}), ['id']),
                                 courseTopicContentId: createdCourseTopic.id,
-                                curriculumTopicAssessmentInfoId: curriculumTopic.id,
+                                curriculumTopicAssessmentInfoId: topicAssessmentInfo.id,
                             });
                         }
 
@@ -392,7 +392,7 @@ class CourseController {
                                 const createFromCurriculum = {
                                     ..._.omit(questionAssessmentInfo.get({plain: true}), ['id']),
                                     courseWWTopicQuestionId: createdCourseQuestion.id,
-                                    curriculumQuestionAssessmentInfoId: curriculumQuestion.id,
+                                    curriculumQuestionAssessmentInfoId: questionAssessmentInfo.id,
                                 };
 
                                 await CourseQuestionAssessmentInfo.create(createFromCurriculum);

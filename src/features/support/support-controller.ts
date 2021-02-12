014ed1dddfce53a7d62fd44e3b46589f20f760e5
red-tt-bear <red.tt.bear@gmail.com>
2021-02-12 10:33:30 -0500

    RED-484 added enhanced error handling and typing
    Added missed debug flag logic
    Added better return type
    Added Error handling to give a more clear error message

--- src/features/support/support-controller.ts
+++ src/features/support/support-controller.ts
@@ -3,6 +3,7 @@ import { RederlyJiraTicketOptions, IssueType } from './support-types';
 
 import configurations from '../../configurations';
 import logger from '../../utilities/logger';
+import WrappedError from '../../exceptions/wrapped-error';
 class SupportController {
     // Initialize
     jira: JiraApi = new JiraApi({
@@ -14,9 +15,8 @@ class SupportController {
         strictSSL: configurations.jira.strictSSL
     });
 
-    createIssue({ summary = 'NO SUMMARY PROVIDED', projectKey = configurations.jira.projectKey, issueType = IssueType.STORY, description = 'NO DESCRIPTION PROVIDED' }: RederlyJiraTicketOptions): unknown {
-        // https://developer.atlassian.com/server/jira/platform/jira-rest-api-examples/#:~:text=Creating%20an%20issue%20using%20the,ID%20of%20the%20issue%20type.
-        return this.jira.addNewIssue({
+    async createIssue({ summary = 'NO SUMMARY PROVIDED', projectKey = configurations.jira.projectKey, issueType = IssueType.STORY, description = 'NO DESCRIPTION PROVIDED' }: RederlyJiraTicketOptions): Promise<JiraApi.JsonResponse | null> {
+        const newIssueOptions = {
             fields: {
                 project:
                 {
@@ -32,6 +32,12 @@ class SupportController {
         if (!configurations.jira.enabled) {
             logger.info(JSON.stringify(newIssueOptions, null, 2));
             return null;
+        }
+        try {
+            // https://developer.atlassian.com/server/jira/platform/jira-rest-api-examples/#:~:text=Creating%20an%20issue%20using%20the,ID%20of%20the%20issue%20type.
+            return await this.jira.addNewIssue(newIssueOptions);
+        } catch (e) {
+            throw new WrappedError('Could not create support ticket', e);
         }
     }
 }
